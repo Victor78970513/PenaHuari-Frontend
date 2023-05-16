e8ac4c223c4c102d372af65646aa820d1ac05081
samu <samuelantoni2002@gmail.com>
2023-05-16 00:14:36 -0400

    correcciones en el disenio

--- src/components/NavigationFrame.jsx
+++ src/components/NavigationFrame.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import { useState } from "react";
 import './NavigationFrame.css'
-import {AiOutlineHome, AiOutlineUser} from "react-icons/ai"
-import {GiForkKnifeSpoon} from 'react-icons/gi';
-import { MdOutlineTableBar } from "react-icons/md";
+import { Icon } from '@iconify/react';
 
 //con el useState pudimos mantener los colores
 export const NavigationFrame = () => {
@@ -24,7 +22,9 @@ export const NavigationFrame = () => {
                   href="#"
                   className={`nav-link ${activeOption === 'home' ? 'active-link' : ''}`}
                   onClick={() => handleOptionClick('home')}
-                ><AiOutlineHome/> Home
+                ><span class="material-symbols-outlined">
+                home
+                </span><span> Home</span>
                 </a>
               </li>
 
@@ -33,7 +33,7 @@ export const NavigationFrame = () => {
                   href="#"
                   className={`nav-link ${activeOption === 'dish' ? 'active-link' : ''}`}
                   onClick={() => handleOptionClick('dish')}
-                ><GiForkKnifeSpoon/> Dish
+                ><Icon className='icono' icon="fluent-emoji-high-contrast:fork-and-knife-with-plate" /><span> Dish</span>
                 </a>
               </li>
               <li className={`nav-item ${activeOption === 'table' ? 'active' : ''}`}>
@@ -41,7 +41,9 @@ export const NavigationFrame = () => {
                   href="#"
                   className={`nav-link ${activeOption === 'table' ? 'active-link' : ''}`}
                   onClick={() => handleOptionClick('table')}
-                ><MdOutlineTableBar/> Table
+                ><span class="material-symbols-outlined">
+                table_bar
+                </span><span> Table</span>
                 </a>
               </li>
               <li className={`nav-item ${activeOption === 'profile' ? 'active' : ''}`}>
@@ -49,7 +51,9 @@ export const NavigationFrame = () => {
                   href="#"
                   className={`nav-link ${activeOption === 'profile' ? 'active-link' : ''}`}
                   onClick={() => handleOptionClick('profile')}
-                ><AiOutlineHome/> Profile
+                ><span class="material-symbols-outlined">
+                person
+                </span><span> Profile</span>
                 </a>
               </li>
             </ul>
