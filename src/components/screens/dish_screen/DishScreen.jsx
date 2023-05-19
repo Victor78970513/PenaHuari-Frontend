36aa5042132ffd8cc62a50559ca1505ac15834fa
Victor78970513 <yer29.chok@gmail.com>
2023-05-18 20:02:22 -0400

    feat Arquitectura de Navegacion

---
+++ src/components/screens/dish_screen/DishScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./DishScreen.css"
+const DishScreen = () => {
+  return (
+    <div>DishScreen</div>
+  )
+}
+
+export default DishScreen
---
+++ src/components/screens/profile_screen/ProfileScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./ProfileScreen.css"
+const ProfileScreen = () => {
+  return (
+    <div>ProfileScreen</div>
+  )
+}
+
+export default ProfileScreen
