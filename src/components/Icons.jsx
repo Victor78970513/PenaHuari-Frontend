52fcc46bfefb1923867d2d0e01db7ecaba1a770f
JamesFlores12 <jaimefloresfernandez60@gmail.com>
2023-05-25 08:33:44 -0400

    Trabajo 1

---
+++ src/components/Icons.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Icons.css"
+export const Title = (props) => {
+  const icons=props.icons
+  return (
+    <div className='icons_container'>
+      {
+        <div className='icons_container1'>
+          </div>
+      }
+
+
+
+
+      </div> 
+   
+  )
+  
+}
---
+++ src/components/Title.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Title.css"
+export const Title = (props) => {
+  const title=props.title
+  return (
+    <div className='title_container'>
+        <h3>Peña-Res</h3>
+       
+        
+
+    </div>
+    
+   
+  )
+  
+}
+
