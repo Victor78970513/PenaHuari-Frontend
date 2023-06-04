fb5a38001a0128936dcc2ffc95ec706796322567
Victor <Yer59.chok@gmail.com>
2023-06-04 10:51:31 -0400

    feat/OrganizacionDeCarpetas-Victor

--- src/components/screens/dish_screen/DishScreen.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./DishScreen.css"
-const DishScreen = () => {
-  return (
-    <div>DishScreen</div>
-  )
-}
-
-export default DishScreen
--- src/components/screens/home_screen/HomeScreen.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./HomeScreen.css"
-const HomeScreen = () => {
-  return (
-    <div>HomeScreen</div>
-  )
-}
-
-export default HomeScreen
