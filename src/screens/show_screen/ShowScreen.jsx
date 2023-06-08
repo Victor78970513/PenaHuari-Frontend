d9d1eb1356609b668b29eecb4aad07fe5f9a44c8
JFoxK29 <jfkontiveros@hotmail.com>
2023-06-08 12:01:51 -0400

    Pantalla de reservacion de mesas

--- src/screens/show_screen/ShowScreen.jsx
+++ src/screens/show_screen/ShowScreen.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Boton from "../../components/atoms/Boton";
 import ShowsContainer from "../../components/atoms/ShowsContainer";
-import Arrow from "../../static/icons/arrow_back.svg";
+import BotonRegresar from "../../static/icons/arrow_back.svg";
 import "./ShowScreen.css";
 
 const ShowScreen = () => {
   return (
     <div className="show-screen">
       <div className="show-title-area">
-        <Boton icono={Arrow} type={"boton-arrow"} name_class={"boton-arrow"} />
+        <Boton icono={BotonRegresar} type={"boton-arrow"} name_class={"boton-arrow"} />
         <h4>Shows</h4>
       </div>
       <ShowsContainer />
