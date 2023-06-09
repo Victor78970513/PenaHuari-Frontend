eedbb19f8edc3af825889849f78e0589916366d1
Victor78970513 <yer29.chok@gmail.com>
2023-06-09 12:22:00 -0400

    fixed/Routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,34 @@ import DishScreen from "./screens/dish_screen/DishScreen";
 import TableScreen from "./screens/table_screen/TableScreen";
 import ProfileScreen from "./screens/profile_screen/ProfileScreen";
 import Breakfast_screen from "./screens/Breakfast_screen/BreakfastScreen";
+import LunchScreen from "./screens/Lunch_screen/LunchScreen";
+import DinnerScreen from "./screens/Dinner_screen/DinnerScreen";
+import DessertScreen from "./screens/Dessert_screen/DessertScreen";
+import DrinksScreen from "./screens/Drinks_screen/DrinksScreen";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomeScreen/>} />
+      <Route path="/" element={<HomeScreen />} />
       <Route path="/dish" element={<DishScreen />} />
       <Route path="/table" element={<TableScreen />} />
       <Route path="/profile" element={<ProfileScreen />} />
-      <Route path="/Breakfast" element={<Breakfast_screen />} />
+      <Route path="/seccion/breakfast" element={<Breakfast_screen />} />
+      <Route path="/seccion/lunch" element={<LunchScreen />} />
+      <Route path="/seccion/dinner" element={<DinnerScreen />} />
+      <Route path="/seccion/dessert" element={<DessertScreen />} />
+      <Route path="/seccion/drinks" element={<DrinksScreen />} />
     </Routes>
   );
 }
 
-
-function Root(){
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
+function Root() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 }
 
 export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Root from './App.jsx'
-import BreakfastScreen from './screens/Breakfast_screen/BreakfastScreen'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LunchScreen from './screens/Lunch_screen/LunchScreen'
-import DinnerScreen from './screens/Dinner_screen/DinnerScreen'
-import DessertScreen from './screens/Dessert_screen/DessertScreen'
-import DrinksScreen from './screens/Drinks_screen/DrinksScreen'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Root from "./App.jsx";
+import "./index.css";
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    // <App /> 
-    // <Breakfast_screen/>
-    //  <Root/> 
-    
-    <BrowserRouter>
-      
-      <Routes>
-        <Route path='/' element={<BreakfastScreen/>} />
-        <Route path='/Lunch' element={<LunchScreen/>} />
-        <Route path='/Dinner' element={<DinnerScreen/>} />
-        <Route path='/Dessert' element={<DessertScreen/>} /> 
-        <Route path='/Drinks' element={<DrinksScreen/>} /> 
-        
-      </Routes>
-      
-
-    </BrowserRouter>,
-  
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);
