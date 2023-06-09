aa9c572c38892a20382b72818654f81b1802f368
Victor78970513 <yer29.chok@gmail.com>
2023-06-09 11:07:24 -0400

    fixed/Screens

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,24 @@ import DishScreen from "./screens/dish_screen/DishScreen";
 import TableScreen from "./screens/table_screen/TableScreen";
 import ProfileScreen from "./screens/profile_screen/ProfileScreen";
 import "./App.css";
-import BarrasContainer from "./components/molecules/BarrasContainer";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomeScreen />} />
-      <Route path="/dish" element={<DishScreen />} />
+      <Route path="/dish" element={<DishScreen/>} />
       <Route path="/table" element={<TableScreen />} />
       <Route path="/profile" element={<ProfileScreen />} />
     </Routes>
   );
 }
 
-
-function Root(){
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
+function Root() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 }
 
 export default Root;
---
+++ src/screens/dish_screen/DishScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DishScreen = () => {
+  return (
+    <div>DishScreen</div>
+  )
+}
+
+export default DishScreen
