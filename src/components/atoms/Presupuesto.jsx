1bc1f5c670ac5ec9eb4ce2b6bc4824ce4f4bbf3d
Victor78970513 <yer29.chok@gmail.com>
2023-06-16 11:00:10 -0400

    feat: final del proyecto

--- src/components/atoms/Presupuesto.jsx
+++ src/components/atoms/Presupuesto.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import IconButton from "./IconButton.jsx";
 import "./Presupuesto.css";
+import { NavLink } from "react-router-dom";
 const Presupuesto = ({ onChange, value, name ,onClick}) => {
   return (
     <div className="presupuesto-container">
@@ -15,6 +16,7 @@ const Presupuesto = ({ onChange, value, name ,onClick}) => {
         />
       </div>
       <IconButton css={"presupuesto-icon"} icon={"solar:hand-money-linear"} onClick={onClick}/>
+      <a href="https://landbot.online/v3/H-1632889-RBC3254X8BX4IUV2/index.html"><IconButton css={"presupuesto-icon"} icon={"fluent:chat-32-filled"}/></a>
     </div>
   );
 };
